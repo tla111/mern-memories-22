05f910d7e9423e2f74ed51a9029bfce30dfa887b
Tim La <tlamarketing111@gmail.com>
2022-03-18 11:40:06 -0400

    created logic for api request to delete post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,4 @@ const url = "http://localhost:5000";
 export const fetchPosts = () => axios.get(`${url}/posts`);
 export const createPost = (newPost) => axios.post(`${url}/posts`, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/posts/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/posts/${id}`);
