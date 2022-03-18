944397f12a1691221fc23a74811f20a84992d0c9
Tim La <tlamarketing111@gmail.com>
2022-03-18 11:30:10 -0400

    clear function working properly

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -23,10 +23,12 @@ const Form = ({ currentId, setCurrentId }) => {
         } else {
             dispatch(createPost(postData));
         }
+        clear();
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({ creator: "", title: "", message: "", tags: "" })
     }
 
     return (
