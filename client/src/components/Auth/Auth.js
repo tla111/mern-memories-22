05840598ad370034e163528a6763e7e86a1ae16d
Tim La <tlamarketing111@gmail.com>
2022-03-20 13:37:25 -0400

    created input for password

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
@@ -6,8 +6,11 @@ import Input from './Input';
 
 const Auth = () => {
     const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false);
 
     const isSignup = false;
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
     const handleSubmit = () => {
 
@@ -32,6 +35,8 @@ const Auth = () => {
                                 <Input name="lastName" label="Last Name" handleChange={handleChange} half />
                             </>
                         )}
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
                     </Grid>
                 </form>
             </Paper>
