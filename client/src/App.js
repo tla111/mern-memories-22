5596237391a5861ab45e1320352e6ef39558ad47
Tim La <tlamarketing111@gmail.com>
2022-03-20 11:53:42 -0400

    organized app, added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import memories from './images/memories.png';
-import useStyles from './styles';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
-const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch])
+const App = () => (
 
-    return (
+    <BrowserRouter>
         <Container maxwidth="lg">
             <Navbar />
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                {/* <Route path="/auth" exact element={<Auth />} /> */}
+            </Routes>
         </Container>
-    )
-}
+    </BrowserRouter>
+
+)
 
 export default App;
