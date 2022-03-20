c9965f2ff130334028660a873977c9ec18f2fa95
Tim La <tlamarketing111@gmail.com>
2022-03-20 11:34:52 -0400

    created logic for navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
 import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -19,10 +20,7 @@ const App = () => {
 
     return (
         <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
+            <Navbar />
             <Grow in>
                 <Container>
                     <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
