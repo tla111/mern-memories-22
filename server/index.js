28d939a2f7734a17eb8e0d49edae6b0016139728
Tim La <tlamarketing111@gmail.com>
2022-03-22 11:13:50 -0400

    created logic for signin backend

--- server/index.js
+++ server/index.js
@@ -78,7 +78,23 @@ app.patch("/posts/:id/likePost", async (req, res) => {
 });
 
 app.post("/user/signin", async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+        const existingUser = await UserModel.findOne({ email });
+
+        if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, "test", { expiresIn: "1h" });
+
+        res.status(200).json({ result: existingUser, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
 });
 
 app.post("/user/signup", async (req, res) => {
