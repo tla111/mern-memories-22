8fabc78cbf574a52136085b19766d5ef35028ca9
Tim La <tlamarketing111@gmail.com>
2022-03-22 11:35:12 -0400

    refactored api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const url = "http://localhost:5000";
+const API = axios.create({ baseURL: "http://localhost:5000" })
 
-export const fetchPosts = () => axios.get(`${url}/posts`);
-export const createPost = (newPost) => axios.post(`${url}/posts`, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/posts/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/posts/${id}`);
-export const likePost = (id) => axios.patch(`${url}/posts/${id}/likePost`);
+
+export const fetchPosts = () => API.get(`/posts`);
+export const createPost = (newPost) => API.post(`/posts`, newPost);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
