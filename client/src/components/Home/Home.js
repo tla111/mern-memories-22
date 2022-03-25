497bb8c7247a4f2bab1bcb7e6776a7aae4d5df09
Tim La <tlamarketing111@gmail.com>
2022-03-25 12:59:15 -0400

    completed project

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid, Paper } from '@material-ui/core';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button, Chip } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+
 import { getPosts } from '../../actions/posts';
 import Pagination from '../Pagination';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import useStyles from './styles';
+
 
 const Home = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -19,12 +21,12 @@ const Home = () => {
     return (
         <div>
             <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
+                <Container maxWidth="xl">
+                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+                        <Grid item xs={12} sm={6} md={9}>
                             <Posts setCurrentId={setCurrentId} />
                         </Grid>
-                        <Grid item xs={12} sm={4}>
+                        <Grid item xs={12} sm={6} md={3}>
                             <Form currentId={currentId} setCurrentId={setCurrentId} />
                             <Paper elevation={6}>
                                 <Pagination />
