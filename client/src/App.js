ca086a0b6a34c9fd3e16be07d7125a470ffbe90b
Tim La <tlamarketing111@gmail.com>
2022-03-25 12:26:39 -0400

    learned how to replace redirect with navigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,29 @@
 import React, { useEffect } from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 
-function Redirect({ to }) {
-    let navigate = useNavigate();
-    useEffect(() => {
-        navigate(to);
-    });
-    return null;
+
+const App = () => {
+
+    return (
+        <BrowserRouter>
+            <Container maxwidth="xl">
+                <Navbar />
+                <Routes>
+                    <Route path="/" exact element={<Navigate replace to="/posts" />} />
+                    <Route path="/posts" exact element={<Home />} />
+                    <Route path="/posts/search" exact element={<Home />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/auth" exact element={<Auth />} />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    )
 }
-
-const App = () => (
-
-    <BrowserRouter>
-        <Container maxwidth="xl">
-            <Navbar />
-            <Routes>
-                <Route path="/" exact element={() => <Redirect to="/posts" />} />
-                <Route path="/posts" exact element={<Home />} />
-                <Route path="/posts/search" exact element={<Home />} />
-                <Route path="/posts/:id" element={<PostDetails />} />
-                <Route path="/auth" exact element={<Auth />} />
-            </Routes>
-        </Container>
-    </BrowserRouter>
-
-)
 
 export default App;
